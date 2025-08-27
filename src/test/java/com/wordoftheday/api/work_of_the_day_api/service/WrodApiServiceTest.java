d542d377af848f1b00018c60a901fcc4edaeae1e
Shuaib <shuaib6950@gmail.com>
2025-08-27 23:27:49 +0600

    add test class

---
+++ src/test/java/com/wordoftheday/api/work_of_the_day_api/service/WrodApiServiceTest.java
@@ -0,0 +1,23 @@
+package com.wordoftheday.api.work_of_the_day_api.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class WrodApiServiceTest {
+
+    @Test
+    void fetchRandomWord() {
+    }
+
+    @Test
+    void fetchWordDefinition() {
+    }
+
+    @Test
+    void getRandomWordWithDefinition() {
+    }
+
+    @Test
+    void clearWordOfTheDayCache() {
+    }
+}
